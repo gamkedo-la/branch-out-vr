98e072ca1630086d3880366bbd3135b01feaf6b8
CTGabbianelli <108819775+CTGabbianelli11@users.noreply.github.com>
2024-11-06 20:49:37 -0500

    Added Tertiary and Leaf nodes

---
+++ Assets/Scripts/Tree/Christian'sTestFolder/Leaf.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Leaf : TreeLimbBase
+{
+    float percentageToEndBranch;
+
+
+    //Initialize if spawned from a branch
+
+    public void Initialize(UnityEvent growEvent, TertiaryBranch previousTertiaryBranch)
+    {
+        base.Initialize(growEvent);
+        this.previousLimb = previousTertiaryBranch;
+        if (previousTertiaryBranch != null)
+        {
+            transform.localEulerAngles = GetRandomRotations();
+        }
+        Initialize();
+
+    } 
+    void Initialize()
+    {
+        //tertiaryBranchPrefab = limbContainer.tertiaryBranch;
+        //percentageToEndBranch = Random.value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Grow()
+    {
+        base.Grow();
+    }
+
+}
---
+++ Assets/Scripts/Tree/Christian'sTestFolder/TertiaryBranch.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TertiaryBranch : TreeLimbBase
+{
+    TertiaryBranch tertiaryBranchPrefab;
+    Leaf leafPrefab;
+    float percentageToEndBranch;
+
+
+    //Initialize if spawned from a branch
+
+    public void Initialize(UnityEvent growEvent, TertiaryBranch previousTertiaryBranch)
+    {
+        base.Initialize(growEvent);
+        this.previousLimb = previousTertiaryBranch;
+        if (previousTertiaryBranch != null)
+        {
+            transform.localEulerAngles = GetRandomRotations();
+        }
+        Initialize();
+
+    } 
+    //Initialize if spawned from a trunk
+    public void Initialize(UnityEvent growEvent, SecondaryBranch previousSecondaryBranch)
+    {
+        base.Initialize(growEvent);
+        this.previousLimb = previousSecondaryBranch;
+        if (previousSecondaryBranch != null)
+        {
+            transform.localEulerAngles = GetRandomRotations();
+        }
+        Initialize();
+
+    }
+    void Initialize()
+    {
+        tertiaryBranchPrefab = limbContainer.tertiaryBranch;
+        leafPrefab = limbContainer.leaf;
+        percentageToEndBranch = Random.value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void HandleBranches()
+    {
+        TreeLimbBase limb = Instantiate(leafPrefab, GetRandomPositionOnLimb(), Quaternion.Euler(GetRandomRotations()), transform);
+        branchedLimbs.Add(limb);
+        (limb as Leaf).Initialize(GrowthHappenedEvent, this);
+    }
+
+    public override void Grow()
+    {
+        base.Grow();
+
+        //if (WillGrowSub())
+           // HandleBranches();
+
+        if (nextLimb == null && WillLimbContinue())
+        {
+            TreeLimbBase limb = Instantiate(leafPrefab, top.position, Quaternion.Euler(GetRandomRotations()), transform);
+            nextLimb = (limb);
+            (limb as Leaf).Initialize(GrowthHappenedEvent, this);
+        }
+
+    }
+
+}
