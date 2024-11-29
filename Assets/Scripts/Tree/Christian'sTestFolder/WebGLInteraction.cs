333f0437c7de4d1eb4a9a20dc3dc4f535d45dd5d
CTGabbianelli <108819775+CTGabbianelli11@users.noreply.github.com>
2024-11-29 14:06:02 -0500

    Added Webgl interaction for testing cutting

---
+++ Assets/Scripts/Tree/Christian'sTestFolder/WebGLInteraction.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WebGLInteraction : MonoBehaviour
+{
+    public Vector3 CurrentPosition;
+    public InputAction useToolAction,screenPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        useToolAction.Enable();
+            useToolAction.performed += _ => Raycast();
+        screenPos.Enable();
+        screenPos.performed += context => { CurrentPosition = context.ReadValue<Vector2>(); };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Raycast()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(CurrentPosition);
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            //suppose i have two objects here named obj1 and obj2.. how do i select obj1 to be transformed 
+            if (hit.transform != null && hit.transform.GetComponent<PhysicalInteraction>())
+            {
+                print(hit.transform.name);
+                hit.transform.GetComponent<PhysicalInteraction>().ClickedOn();
+            }
+        }
+    }
+}
